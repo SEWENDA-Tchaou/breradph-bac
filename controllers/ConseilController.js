3363d96c324e9dcc9677f0c80e13f07600b489b4
sewenda tchaou <sewendatchaou@gmail.com>
2023-11-27 15:38:50 -0800

    add model content on controller

--- controllers/ConseilController.js
+++ controllers/ConseilController.js
@@ -1,4 +1,30 @@
 // import Crud from "../models/ConseilModel.js";
+import db from "../config/Database.js";
+
+const {DataTypes} = Sequelize;
+
+const Crud = db.define('crudconseils', {
+    titre: {
+        type: DataTypes.STRING
+    },
+    conseil1: {
+        type: DataTypes.STRING
+    },
+    conseil2: {
+        type: DataTypes.STRING
+    },
+    conseil3:{ 
+        type: DataTypes.STRING
+    }
+
+});
+
+export default Crud;
+
+(async() => {
+    await db.sync();
+}) ();
+
 
 
 export const getContents = async(req, res) => {
