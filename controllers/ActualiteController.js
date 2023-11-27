530366f6cb818d7e20951e714a641050685b2497
sewenda tchaou <sewendatchaou@gmail.com>
2023-11-27 15:42:03 -0800

    remove model content on controller

--- controllers/ActualiteController.js
+++ controllers/ActualiteController.js
@@ -1,35 +1,6 @@
-// import Crud from "../models/ActualiteModel.js";
+import Crud from "../models/ActualiteModel.js";
 import path from 'path';
 import fs from 'fs';
-import db from "../config/Database.js";
-
-const {DataTypes} = Sequelize;
-
-const Crud = db.define('actualites', {
-    image: {
-        type: DataTypes.STRING
-    },
-    titre: {
-        type: DataTypes.STRING
-    },
-    theme: {
-        type: DataTypes.STRING
-    },
-    themeTout: {
-        type: DataTypes.STRING
-    },
-    url:{ 
-        type: DataTypes.STRING
-    }
-
-});
-
-export default Crud;
-
-(async() => {
-    await db.sync();
-}) ();
-
 
 export const getContents = async(req, res) => {
    try {
--- controllers/ConseilController.js
+++ controllers/ConseilController.js
@@ -1,30 +1,4 @@
-// import Crud from "../models/ConseilModel.js";
-import db from "../config/Database.js";
-
-const {DataTypes} = Sequelize;
-
-const Crud = db.define('crudconseils', {
-    titre: {
-        type: DataTypes.STRING
-    },
-    conseil1: {
-        type: DataTypes.STRING
-    },
-    conseil2: {
-        type: DataTypes.STRING
-    },
-    conseil3:{ 
-        type: DataTypes.STRING
-    }
-
-});
-
-export default Crud;
-
-(async() => {
-    await db.sync();
-}) ();
-
+import Crud from "../models/ConseilModel.js";
 
 
 export const getContents = async(req, res) => {
